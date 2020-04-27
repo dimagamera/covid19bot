1f309525ebbc6553622317baeeb0ac011c02f393
dimagamera <note.dima.gamera@gmail.com>
2020-04-27 16:09:46 +0400

    first commit

---
+++ coronabot/lib/settings.py
@@ -0,0 +1,7 @@
+############## TELEGRAM BOT ########################
+token = "1113083009:AAEHdsPlQ38p6DO_pJ2d8zWNpVgSJFx7REI"
+############## MSQL DATABASES ######################
+host = "localhost"
+user = "root"
+passwd = ""
+database = ""
