f66eb1b4378d67cbb2f37f4b6c29550194de0743
dimagamera <note.dima.gamera@gmail.com>
2020-04-29 13:20:23 +0400

    Update

--- lib/settings.py
+++ lib/settings.py
@@ -1,7 +1,7 @@
 ############## TELEGRAM BOT ########################
 token = "1113083009:AAEHdsPlQ38p6DO_pJ2d8zWNpVgSJFx7REI"
 ############## MSQL DATABASES ######################
-host = "13.92.93.63"
+host = "104.211.13.232"
 user = "root"
 passwd = "my-secret-root"
 database = ""
