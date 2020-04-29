e97efe0dff1832b97b2c9f1c7ae6f04919efe470
dimagamera <note.dima.gamera@gmail.com>
2020-04-29 14:18:47 +0400

    Origin Master

--- start.py
+++ start.py
@@ -6,13 +6,13 @@ __URL = "https://api.covid19api.com/summary"
 
 db_object = db_manager(host, user, passwd, database, __URL)
 
-# covid_19_data = db_object.get_all_data()
-# covid_19_save = db_object.save_all_data(covid_19_data)
+covid_19_data = db_object.get_all_data()
+covid_19_save = db_object.save_all_data(covid_19_data)
 
 bot = telebot.TeleBot(token)
 
-# covid_19_data
-# covid_19_save
+covid_19_data
+covid_19_save
 
 
 @bot.message_handler(commands=['start'])
