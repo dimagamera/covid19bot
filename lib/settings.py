9525cd14af09f072d83f7a85fee990ce8943c83a
dimagamera <note.dima.gamera@gmail.com>
2020-05-02 15:35:49 +0400

    Update

--- lib/settings.py
+++ lib/settings.py
@@ -1,6 +1,6 @@
 ############## TELEGRAM BOT ########################
 token = "1113083009:AAEHdsPlQ38p6DO_pJ2d8zWNpVgSJFx7REI"
 ############## MSQL DATABASES ######################
-host = "104.211.0.221"
+host = "13.48.24.227"
 user = "root"
 passwd = "my-secret-pass"
--- start.py
+++ start.py
@@ -6,13 +6,13 @@ __URL = "https://api.covid19api.com/summary"
 
 db_object = db_manager(host, user, passwd, __URL)
 
-# covid_19_data = db_object.get_all_data()
-# covid_19_save = db_object.save_all_data(covid_19_data)
+covid_19_data = db_object.get_all_data()
+covid_19_save = db_object.save_all_data(covid_19_data)
 
 bot = telebot.TeleBot(token)
 
-# covid_19_data
-# covid_19_save
+covid_19_data
+covid_19_save
 
 
 @bot.message_handler(commands=['start'])
@@ -41,8 +41,7 @@ def get_text_messages(message):
                 item[6]) + "\n☠️ Кількість смертей ☠️ → " + str(item[7]) + "\n☠️ Кількість смертей за добу ☠️ → " + str(item[6]) + "\n💊 Кількість вилікуваних 💊 → " + str(item[9]) + "\n💊 Кількість вилікуваних за добу 💊 → " + str(item[8]), reply_markup=keyboard)
 
     if message.text == "Симптоми COVID-19":
-        bot.send_photo(
-            message.chat.id, "http://i.piccy.info/i9/7333fbd68a014010173ed8f1a74969f0/1587472373/401901/1374142/43_Algorytm_d_1110_i_za_p_1110_dozry_COVID_19.jpg", reply_markup=keyboard)
+        bot.send_photo(message.chat.id, "http://i.piccy.info/i9/7333fbd68a014010173ed8f1a74969f0/1587472373/401901/1374142/43_Algorytm_d_1110_i_za_p_1110_dozry_COVID_19.jpg", reply_markup=keyboard)
     elif message.text == "Як користуватись ботом?":
         bot.send_message(
             message.chat.id, "1️⃣Щоб переглянути статистику, напишіть назву країни. Наприклад: 𝐔𝐤𝐫𝐚𝐢𝐧𝐞, 𝐈𝐭𝐚𝐥𝐲, 𝐂𝐡𝐢𝐧𝐚, 𝐑𝐮𝐬𝐬𝐢𝐚𝐧 𝐅𝐞𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧, або по коду 𝐔𝐀, 𝐈𝐓, 𝐂𝐍, 𝐑𝐔", reply_markup=keyboard)
