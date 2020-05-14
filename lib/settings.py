8be8da2b018fb67add38bc044003fdc9ae8c7b0a
dimagamera <46674070+dimagamera@users.noreply.github.com>
2020-05-14 13:09:56 +0300

    Update settings.py

--- lib/settings.py
+++ lib/settings.py
@@ -1,6 +1,6 @@
 ############## TELEGRAM BOT ########################
 token = "1113083009:AAEHdsPlQ38p6DO_pJ2d8zWNpVgSJFx7REI"
 ############## MSQL DATABASES ######################
-host = "40.121.194.112"
-user = "wordpress"
-passwd = "wordpress"
+host = "172-31-30-132"
+user = "root"
+passwd = "my-secret-pass"
