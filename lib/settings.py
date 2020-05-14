2ab8e5e705796ac23a8690792943f72605e50e45
dimagamera <46674070+dimagamera@users.noreply.github.com>
2020-05-14 15:02:44 +0300

    Update settings.py

--- lib/settings.py
+++ lib/settings.py
@@ -1,6 +1,6 @@
 ############## TELEGRAM BOT ########################
 token = "1113083009:AAEHdsPlQ38p6DO_pJ2d8zWNpVgSJFx7REI"
 ############## MSQL DATABASES ######################
-host = "13.53.75.27"
-user = "root"
-passwd = "my-secret-pass"
+host = "40.121.194.112"
+user = "root2"
+passwd = "root2"
